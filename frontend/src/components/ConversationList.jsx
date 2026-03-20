e65d08e2cd78903961e684c73234545108608cf8
saradir <120840462+saradir@users.noreply.github.com>
2026-03-20 17:47:52 +0200

    add conversations list component

--- frontend/src/components/ConversationList.jsx
+++ frontend/src/components/ConversationList.jsx
@@ -1,8 +1,23 @@
 
 function ConversationList({ conversations }){
 
+    if(!conversations || conversations.length === 0) return <p>No chats yet</p>
+
     return(
-        
+
+        <div className="conversation-list">
+            { conversations.map( c => (
+                <div key={c.id} className="conversation" id={c.id}>
+                    <div className="conversation-title">
+                        {c.participants}
+                    </div>
+
+                    <div className="conversation-preview">
+                        {c.lastMessage?.content || ""}
+                    </div>
+                </div>
+            ))}
+        </div>     
     )
 }
 
--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,13 +1,51 @@
 import { useState, useEffect } from "react";
+import ConversationList from "../components/ConversationList";
 
 
 export default function Homepage(){
 
-   // const [conversations, setConversations] = useState();
+   const [conversations, setConversations] = useState(null);
+   const [loading, setLoading] = useState(true);
+   const [error, setError] = useState('');
 
+   useEffect(() => {
+    async function fetchConversations(){
+        try {
+            const response = await fetch(`${import.meta.env.VITE_API_SERVER}/conversations`,{
+                credentials: "include"
+            });
+    
+            if(!response.ok){
+                setError("Failed to retrieve conversations");
+                console.error("Fetch failed: ", response.status)
+                return;
+            }
+
+            const data = await response.json();
+            setConversations(data.data)   
+        } catch (err) {
+            setError("Failed to retrieve conversations");
+            console.error("Error: ", err)
+            
+        } finally{
+            setLoading(false);
+        }
+    }
+
+    fetchConversations();
+   }, []);
+
+
+
+   if(loading) return <p>Loading...</p>
+   if(error) return <p>{error}</p>
 
     return(
-        <p>Homepage</p>
+        <div className="main">
+
+            <ConversationList conversations={ conversations }/>
+        </div>
+        
     )
 
 }
