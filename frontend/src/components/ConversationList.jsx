bc1d0f8f8d184b9f2723203a506a7744a36f8847
saradir <120840462+saradir@users.noreply.github.com>
2026-03-23 15:11:07 +0200

    fix: remove particpants array from div, remove id from div

--- frontend/src/components/ConversationList.jsx
+++ frontend/src/components/ConversationList.jsx
@@ -2,14 +2,14 @@
 function ConversationList({ conversations }){
 
     if(!conversations || conversations.length === 0) return <p>No chats yet</p>
-
+  
     return(
 
         <div className="conversation-list">
             { conversations.map( c => (
-                <div key={c.id} className="conversation" id={c.id}>
+                <div key={c.id} className="conversation" >
                     <div className="conversation-title">
-                        {c.participants}
+                        
                     </div>
 
                     <div className="conversation-preview">
