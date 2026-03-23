2e24bfd35c270cb963b63330abc070a4ac1336cf
saradir <120840462+saradir@users.noreply.github.com>
2026-03-23 12:40:38 +0200

    create Contacts page

---
+++ frontend/src/pages/Contacts.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect} from "react"
+import ContactsList
+ from "../components/ContactsList";
+import { SearchForm } from "../components/SearchForm";
+import { SearchResults } from "../components/SearchResults";
+export function Contacts(){
+
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [contacts, setContacts] = useState(null);
+    const [searchResults, setSearchResults] = useState(null);
+    const [searchMode, setSearchMode] = useState(false);
+
+
+    async function handleSearch(query){
+
+        setSearchMode(true);
+
+        try{
+            const response = await fetch(`${import.meta.env.VITE_API_SERVER}/contacts/search?q=${query}`, {
+                credentials: "include"
+            });
+
+            if(!response.ok){
+                setError("Something went wrong. Try to refresh the page");
+                console.error("Search failed with code ", response.status);
+            }
+
+            const data = await response.json();
+            setSearchResults(data.data);
+        } catch (err){
+            setError(err);
+            console.error("Error: ", err);
+        }
+    }
+
+
+
+
+
+    useEffect(() => {
+        async function fetchContacts(){
+            try{
+                const response = await fetch(`${import.meta.env.VITE_API_SERVER}/contacts`,{
+                credentials: "include"
+                });
+
+                if(!response.ok){
+                    setError("Failed to fetch contacts");
+                    console.error(response.status, ": Failed to fetch contacts");
+                }
+
+                const data = await response.json();
+                setContacts(data.data);
+            } catch (err){
+                setError(err);
+                console.error("Error: ", err);
+            } finally{
+                setLoading(false);
+            }
+        }
+        
+        fetchContacts();
+    }, []);
+
+    if(error) return <p>{error}</p>
+    if(loading) return <p>Loading...</p>
+
+    return(
+        <div className="contacts-page">
+
+            <SearchForm handleSearch={handleSearch} setSearchMode={setSearchMode} searchMode={searchMode}  />
+
+            {searchMode
+            ?<SearchResults contacts={searchResults} />
+            :<ContactsList contacts={contacts} />
+            }
+
+        </div>
+    )
+}
