1d6fb8aa76e77ac304b26cc1dc2498a4204aa96a
saradir <120840462+saradir@users.noreply.github.com>
2026-03-23 18:01:34 +0200

    make click on username navigate to conversation page in searchResults

--- frontend/src/components/SearchResults.jsx
+++ frontend/src/components/SearchResults.jsx
@@ -1,15 +1,22 @@
 import { UserRow } from "./UserRow"
 import { startConversation } from "../services/conversations"
+import { useNavigate } from "react-router-dom";
 export function SearchResults({ contacts}){
 
+    const navigate = useNavigate();
     if(!contacts || contacts.length === 0) return <p>No matches found</p>
+
+    async function handleClick(userId){
+        const {id} = await startConversation(userId);
+        navigate(`/conversations/${id}`);
+    }
     
     return(
 
         <div className="search-results">
             { contacts.map( c => (
 
-                <UserRow key={c.id} user={c} onClick={startConversation} />
+                <UserRow key={c.id} user={c} onClick={handleClick} />
             ))}
 
         </div>     
