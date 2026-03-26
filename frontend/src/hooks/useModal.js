7ad08e75bd454ebcb3f422c7b56bc983c5339bb8
saradir <120840462+saradir@users.noreply.github.com>
2026-03-27 01:07:12 +0200

    create hook to use with Modal component

---
+++ frontend/src/hooks/useModal.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export function useModal(){
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    function open(){
+        setIsOpen(true);
+    }
+
+    function close(){
+        setIsOpen(false);
+    }
+
+    return {isOpen, open, close}
+}
