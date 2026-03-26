ac37e27bd71b73a43bd95f588f33cfc5e165a344
saradir <120840462+saradir@users.noreply.github.com>
2026-03-27 01:03:55 +0200

    create ContractProfile component

---
+++ frontend/src/components/ContactProfile.jsx
@@ -0,0 +1,26 @@
+import "../styles/ContactProfile.css"
+
+export function ContactProfile({contact, isInContacts, onAdd}){
+
+    const inContacts = isInContacts(contact.id);
+
+    if (!contact) return null;
+    return (
+        <div className="contact-profile">
+            <div className="personal-details">
+                <div className="contact-name">
+                    Username: {contact.username}
+                </div>
+
+                <div className="email">
+                    Email: {contact.email}
+                </div>               
+            </div>
+
+            <div className="profile-controls">
+                <button className="add-button" disabled={inContacts} onClick={() => onAdd(contact.id)}>Add</button>
+            </div>
+
+        </div>
+    )
+}
