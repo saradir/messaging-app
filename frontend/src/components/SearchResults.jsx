48df6ed5020947803308ce8af2f2f7fc663d4534
saradir <120840462+saradir@users.noreply.github.com>
2026-03-27 01:12:33 +0200

    move click handling into parent

--- frontend/src/components/SearchResults.jsx
+++ frontend/src/components/SearchResults.jsx
@@ -1,18 +1,10 @@
 import { UserRow } from "./UserRow"
-import { startConversation } from "../services/conversations"
-import { useNavigate } from "react-router-dom";
 import "../styles/searchResults.css";
 
 export function SearchResults({ contacts}){
 
-    const navigate = useNavigate();
     if(!contacts || contacts.length === 0) return <div className="empty-state">No matches found</div>
-
-    async function handleClick(userId){
-        const {id} = await startConversation(userId);
-        navigate(`/conversations/${id}`);
-    }
-    
+   
     return(
 
         <div className="search-results">
