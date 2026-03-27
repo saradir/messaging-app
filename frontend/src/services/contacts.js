08cf118e9c894459242029381145686787dde4b2
saradir <120840462+saradir@users.noreply.github.com>
2026-03-27 17:05:08 +0300

    create remove contact service

--- frontend/src/services/contacts.js
+++ frontend/src/services/contacts.js
@@ -36,3 +36,17 @@ export async function fetchContacts(){
     return data.data;
 } 
 
+export async function removeContact(contactId){
+    const response = await fetch(`${import.meta.env.VITE_API_SERVER}/contacts/${contactId}`,{
+        credentials: "include",
+        method: "DELETE",
+    });
+
+    if (!response.ok) {
+        const errorData = await response.json().catch(() => null);
+        throw new Error(errorData?.message || `Operation failed: ${response.status}`);
+    }
+
+    return true;
+}
+
