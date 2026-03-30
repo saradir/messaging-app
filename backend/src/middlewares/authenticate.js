4ba6a22bb19453da64f28efb02692741b4825729
saradir <120840462+saradir@users.noreply.github.com>
2026-03-30 21:16:19 +0300

    add basic authorization to protected routes

--- backend/src/middlewares/authenticate.js
+++ backend/src/middlewares/authenticate.js
@@ -1,26 +1,6 @@
-import sessions from "../tests/sessions.js";
-import users from "../tests/users.js";
-
-export function authenticateUser(req, res, next){
-    const sessionId = req.cookies.sessionId;
-    const session = sessions.find( s => (s.id === sessionId));
-
-    if (!sessionId) {
-        return res.status(401).json({ success: false });
-    }
-
-    if(!session) return res.status(401).json({
-        success: false, 
-        message: "Invalid request"
-    });
-    
-    const user = users.find( u => u.id === session.userId);
-    if(!user) return res.status(401).json({
-        success: false, 
-        message: "Invalid request"
-    });
-
-
-    req.user = user;
-    return next();
+function requireAuth(req, res, next) {
+  if (!req.user) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  next();
 }
