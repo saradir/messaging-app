cbccd6d2d5ada29024dc5e13737824b8662d39c6
saradir <120840462+saradir@users.noreply.github.com>
2026-03-30 22:49:40 +0300

    account for user object with invalid value

--- backend/src/middlewares/authenticate.js
+++ backend/src/middlewares/authenticate.js
@@ -1,5 +1,5 @@
 export function requireAuth(req, res, next) {
-  if (!req.user) {
+  if (!req.user || !Number.isInteger(req.user.id)) {
     return res.status(401).json({ message: "Unauthorized" });
   }
   next();
