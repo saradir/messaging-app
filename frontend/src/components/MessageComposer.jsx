b90581fd347a7fdd5cd51339f84e3fb1b1c066ce
saradir <120840462+saradir@users.noreply.github.com>
2026-04-16 13:48:14 +0300

    feat: allow sending messaging by pressing Enter

--- frontend/src/components/MessageComposer.jsx
+++ frontend/src/components/MessageComposer.jsx
@@ -14,10 +14,16 @@ export function MessageComposer({handleSubmit}){
             console.error(err);
         }
     }
+
+    async function handleKeyDown(e) {
+        if(e.key === "Enter" && !e.shiftKey){
+            e.preventDefault();
+            submitForm(e);
+        }
+    }
     return(       
         <form className="message-input" onSubmit={submitForm}>
-            <textarea name="content" minLength={1} value={input} onChange={e => setInput(e.target.value)}>
-
+            <textarea name="content" minLength={1} value={input} onChange={e => setInput(e.target.value)} onKeyDown={handleKeyDown} />
 
             <button type="submit" className="submit-button" disabled={input.trim().length < 1}>Send</button>
         </form>
