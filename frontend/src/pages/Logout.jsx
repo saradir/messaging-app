3d7bd64422342cbf4c2eb4c16a99ebcaddae9dfc
saradir <120840462+saradir@users.noreply.github.com>
2026-04-21 22:19:28 +0300

    add no caching guards for auth routes

--- frontend/src/pages/Logout.jsx
+++ frontend/src/pages/Logout.jsx
@@ -3,14 +3,12 @@ import { useEffect, useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 
 export function Logout(){
-
     const {setCurrentUser} = useContext(AuthContext);
      
     const navigate = useNavigate();
     useEffect(() => {
         async function logout() {
-        const response = await fetch(`${import.meta.env.VITE_API_SERVER}/auth/logout`, { method: "POST", credentials: "include" });
-
+        const response = await fetch(`${import.meta.env.VITE_API_SERVER}/auth/logout`, { method: "POST", credentials: "include", cache: "no-store" });
         if(!response.ok){
             console.error("Something went wrong");
             return;
