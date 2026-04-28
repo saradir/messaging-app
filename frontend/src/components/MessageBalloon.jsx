61cc184297723713f7ae8a79f91843b97e5642f4
saradir <120840462+saradir@users.noreply.github.com>
2026-04-28 17:10:36 +0300

    remove newline

--- frontend/src/components/MessageBalloon.jsx
+++ frontend/src/components/MessageBalloon.jsx
@@ -1,6 +1,5 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
-
 import "../styles/MessageBalloon.css"
 
 export function MessageBalloon({ message, onResend }){
