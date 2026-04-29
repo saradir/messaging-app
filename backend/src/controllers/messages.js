98fa952616bcafb42244cf12dc1659069a10ad0b
saradir <120840462+saradir@users.noreply.github.com>
2026-04-29 18:37:16 +0300

    emit to room instead of each user

--- backend/src/controllers/messages.js
+++ backend/src/controllers/messages.js
@@ -42,10 +42,9 @@ export async function create(req, res, next){
 
 
 
-        message.conversation.memberships.forEach( m => {
-            io.to(`user:${m.user.id}`).emit("message:new", message);
-            console.log(`message sent to ${m.user.username}`)
-        })
+        io.to(`conversation:${conversationId}`).emit("message:new", message);
+        console.log(`message sent to ${m.user.username}`)
+        
         return res.status(201).json({
             success: true,
             data: message
